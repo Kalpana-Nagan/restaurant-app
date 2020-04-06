d7806c70f2388907d6b7d1c05ed085a857315cb8
Kalpana <kalpana@MLIVE-MAC332.local>
2020-04-06 10:55:12 -0400

    Updates

---
+++ precache-manifest.37573946b911887176547fff5a4a558c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae8bf591798a2bf70ed951e16cfe296",
+    "url": "/restaurant-app/index.html"
+  },
+  {
+    "revision": "b988b78c5fe50ab812af",
+    "url": "/restaurant-app/static/css/main.382e5eda.chunk.css"
+  },
+  {
+    "revision": "0b960b121aeec6e1b455",
+    "url": "/restaurant-app/static/js/2.15b7e116.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant-app/static/js/2.15b7e116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b988b78c5fe50ab812af",
+    "url": "/restaurant-app/static/js/main.75173eaf.chunk.js"
+  },
+  {
+    "revision": "6042aa631f0f22c40118",
+    "url": "/restaurant-app/static/js/runtime-main.f890e963.js"
+  }
+]);
